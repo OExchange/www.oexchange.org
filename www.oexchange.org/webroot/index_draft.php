665b3fb1ed58e4fd81b1ceb32194bd640c1d062e
Will Meyer <will@willmeyer.com>
2010-04-23 22:21:56 -0400

    more home page tweakage

--- www.oexchange.org/webroot/index_draft.php
+++ www.oexchange.org/webroot/index_draft.php
@@ -5,14 +5,15 @@ include 'header.php';
 
 <div class="banner">
 	<h1>Any content, any service.  The web a big place.</h1>
-	<h3>OExchange is an <a href="/spec">open specification</a> for distributed content sharing on the web.</h3>
-	<p>Share URLs with any service on the web.  Locate new services automatically.  Personalize the services available to users.</p>
+	<h4>Share URLs with any service.  Locate new services automatically.  Personalize the services available to users.
+		OExchange is an <a href="/spec">open specification</a> for distributed content sharing on the web.
+	</h4>
 </div>
 	
 <p>
 	Tweeting on Twitter?  Sharing on Facebook?  Sure thing.  But what about posting to your favorite off-beat forum?  Or translating and printing?  What if your browser and your sharing tools could automatically figure out what options make sense for you, as you browse the web?
 </p>
-<p>OExchange is an open specification that defines:</p>
+<p>OExchange defines:</p>
 <ul>
 	<li>A simple method for <b>sending URL-based content to a web-based service</b>.  Send a set of defined parameters via HTTP to a well-defined endpoint.</li>
 	<li>A mechanism for <b>specifying and discovering available services automatically</b>. Find services by hostname, using XRD and /.well-known/host-meta, or from web pages, with page-level link tags.</li>
