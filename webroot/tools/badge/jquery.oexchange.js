fa7ad60053de27e7ccae01e69e673296efa62519
mjk <matt@addthis.com>
2010-05-11 12:17:34 -0400

    - Updated with basic console integration to load user's preferred services (and their definitions), as well as fill the storage cache (relies on postMessage to do the x-domain communication from the console.  - Added static method for grabbing user's services. Since the load is asynchronous, we need to probably make this an asynchronous function -- or just wrap it into more useful higher-level functions.

--- webroot/tools/badge/jquery.oexchange.js
+++ webroot/tools/badge/jquery.oexchange.js
@@ -2,8 +2,95 @@
 * Work-in-progress jQuery plugin for OExchange integration.
 */
 (function () {
-    var addUrl = 'http://ve04.clearspring.local/oxconsole/',
+    var addUrl = '//ve04.clearspring.local/oxconsole/', // XXX need real console up on oexchange.org
+        oexUrl = 'http://www.oexchange.org',
+        supportStorage = window.Storage && window.localStorage,
+        loaded = 0,
+        serviceList = [],
+        serviceHash = {},
+        serviceExport = {},
+        userServices = [],
         xrd;
+
+
+    function reduce( o, fn, acc, cxt ){
+        if ( !o )
+            return acc;
+
+        if ( o instanceof Array )
+            for( var i = 0, len = o.length, v = o[0]; i < len; v = o[++i] )
+                acc = fn.call( cxt || o, acc, v, i, o );
+        else
+            for ( var name in o )
+                acc = fn.call( cxt || o, acc, o[ name ], name, o );
+
+        return acc;
+    }
+
+    function toKV(o, del){
+        return reduce(o, function(acc, v, k){
+            acc.push( encodeURIComponent(k) + '=' + encodeURIComponent(v) ); return acc;
+        }, []).join(del || "&");
+    }
+
+    function fromKV(q, del){
+        return reduce(q.split(del || '&'), function(acc, pair){
+            var kv = pair.split('='), k = kv[0], v = kv[1];
+            if (k) acc[k] = decodeURIComponent(v);
+            return acc;
+        }, {});
+    }
+
+    /**
+    * For postMessage integration. Only loads in the user's service list and definition hash when needed.
+    */
+    function messageHandler(s) {
+        if (!s || addUrl.indexOf(s.origin) !== 0) return;
+        var data = fromKV(s.data);
+        if (data.sl) serviceList = JSON.parse(data.sl);
+        if (data.sh) serviceHash = JSON.parse(data.sh);
+        if (serviceList.length && data.sh) fillPreferredServices();
+    }
+
+    /**
+    * Adds the user's preferred services to the userServices array.
+    * Generally only called once, but can be forced on subsequent loads.
+    */
+    function fillPreferredServices(f) {
+        if ((!f || this.f) && userServices.length) return;
+        else if (f) this.f = 1;
+
+        var svc;
+        for (var i = 0; i < serviceList.length; i++) {
+            var k = serviceList[i],
+                svc = serviceHash[k];
+            if (svc) {
+                svc.code = urlToCode(k);
+                svc.url = svc.offer;
+                serviceExport[svc.code] = {name: svc, code: svc.code, icon: svc.icon, icon32: svc.icon32, url: svc.url};
+                userServices.push(svc);
+            }
+        }
+    }
+
+    /**
+    * Generates a useful service ID from its ad hoc XRD. AddThis oexchange wrappers are detected.
+    */
+    function urlToCode(url) {
+        var code = url.replace(/ /g,'');
+        if (url.indexOf('addthis.com/oexchange')>-1) {
+            // special case
+            code = url.split('oexchange/0.8/xrd/').pop().split('.').shift();
+        }
+        return code;
+    }
+
+    /**
+    * Exposes the user's preferred services array.
+    */
+    function getUserServices() {
+        return userServices;
+    } 
 
     /**
     * Sets a long-lived cookie.
@@ -23,8 +110,7 @@
     */
     function readCookie(k) {
         var c = document.cookie.split(';')
-        for (var i = 0; i < c.length; i++) {
-            var kv = c[i].split('=');
+        for (var i = 0; i < c.length; i++) { var kv = c[i].split('=');
             if (kv.shift() === k) return kv.pop();
         }
     }
@@ -40,7 +126,7 @@
 
             for (var i = 0; i < links.length; i++ ){
                 var l = links[i];
-                if (l.rel && l.rel.match(/http:\/\/oexchange.org\/spec\/0.+\/rel\/related-target/)) {
+                if (l.rel && l.rel.match(/oexchange.org\/spec\/0.+\/rel\/related-target/)) {
                     oex = l.href;
                     break;
                 }
@@ -86,8 +172,9 @@
     /**
     * Opens the "Remember this site" OExchange dialog box.
     * Relatively lame.
+    * @param xrd    xrd to remember; defaults to page XRD
     */
-    function openRememberDialog() {
+    function openRememberDialog(xrd) {
         var div;
         if (!document.getElementById('oexchange-dialog')) {
            var s = ['',
@@ -100,11 +187,11 @@
                     '<button onclick="jQuery.oex.closeDialog();return false">Okay</button>',
                     '</div>',
                     '<div id="oexchange-dialog-content">',
-                    '<p>Oexchange makes it easy to keep track of your favorite places to share. <a href="http://oexchange.org" target="_blank">Learn more</a>.</p>',
+                    '<p>Oexchange makes it easy to keep track of your favorite places to share. <a href="'+oexUrl+'" target="_blank">Learn more</a>.</p>',
                     '<p>Would you like to remember this site for sharing?</p>',
                     '</div>',
                     '<div id="oexchange-dialog-buttons">',
-                    '<input type="hidden" name="add" value="'+(getXRD())+'">',
+                    '<input type="hidden" name="add" value="'+(xrd || getXRD())+'">',
                     '<button onclick="jQuery.oex.saveDialog();jQuery.oex.save()">Save</button>',
                     '<button onclick="jQuery.oex.closeDialog();return false">No Thanks</button>',
                     '</form>',
@@ -136,12 +223,16 @@
         jQuery('#oexchange-dialog').hide();
     }
 
+    function createCommFrame(url) {
+        jQuery('body').append('<iframe id="oexifr'+Math.floor(100*Math.random())+'" src="' + url + '" style="width:1px;height:1px;border:0px" frameborder="0"></iframe>');
+    }
+
     /**
     * Instantiates a 1x1 pixel iframe back to the demo console to save the given OExchange target XRD in the user's preferred list.
     * @param    xrd     an XRD to save; defaults to the page's XRD, if any is listed
     */
     function save(xrd) {
-        jQuery('body').append('<iframe src="' + addUrl + '?add=' + encodeURIComponent(xrd || getXRD()) + '" style="width:1px;height:1px;border:0px" frameborder="0"></iframe>');
+        createCommFrame(addUrl + '?add=' + encodeURIComponent(xrd || getXRD()));
     }
 
     /**
@@ -162,15 +253,16 @@
     /**
     * Renders an OExchange-enabled badge, with hooks back to the console for saving the current page's XRD. 
     */
-    function renderBadge() {
+    function renderBadge(xrd) {
+          xrd = xrd || getXRD();
           this.each(function (i, el){
                 el.innerHTML = ['',
                   '<div class="oexchange-enabled">',
-                  (hasSaved(getXRD()) ?
+                  (hasSaved(xrd) ?
                     '<a class="oexchange-enabled-btn" href="'+addUrl+'" target="_blank">Customize</a>'
                     : 
-                    '<a class="oexchange-enabled-btn" href="#" onclick="jQuery.oex.dopen();return false">Save</a>'),
-                  'This site is <a href="http://oexchange.org" target="_blank">OExchange Enabled</a>.</div>'].join('');
+                    '<a class="oexchange-enabled-btn" href="#" onclick="jQuery.oex.openDialog(\''+xrd+'\');return false">Save</a>'),
+                  'This site is <a href="'+oexUrl+'" target="_blank">OExchange Enabled</a>.</div>'].join('');
                 });
         return this;
     }
@@ -181,11 +273,30 @@
 
     // Add static jQuery methods
     jQuery.oex = {
+        getUserServices : getUserServices,
         save : save,
         showPrompt : promptIfOexchange,
         hidePrompt : hidePrompt,
-        dopen : openRememberDialog,
+        openDialog : openRememberDialog,
         closeDialog : closeDialog,
         saveDialog : saveDialog
     };
+
+    // When the DOM's ready, we check in with the demo console to load the user's preferred services
+    jQuery(document).ready(function() {
+        if (supportStorage) {
+            if (!loaded) {
+                if (jQuery.browser.msie) { 
+                    window.attachEvent('onmessage', messageHandler);
+                } else {
+                    window.addEventListener('message', messageHandler, false);
+                }
+
+                createCommFrame(addUrl+'update-cache.html');
+                createCommFrame(addUrl+'load.html'); // will postMessage back to this calling frame
+                
+                loaded = 1;
+            }
+        }
+    });
 })()
