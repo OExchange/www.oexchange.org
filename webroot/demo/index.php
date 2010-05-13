c1d532e2f2d3976596936587cfb1e3156cd64d7e
foo <foo@clearspring.com>
2010-05-13 14:44:00 -0400

    removed old commented out html from demo page

--- webroot/demo/index.php
+++ webroot/demo/index.php
@@ -37,27 +37,7 @@ include ("../header.php");
     </div>
     <div class="clear"></div>
     
-    <br/>
-    
-    
-    <!--
-    
-	<h3>LinkEater</h3>
-	<p>
-		<a href="./linkeater">LinkEater</a>, an example Target that takes links in a compliant way.  You can use this for testing.
-	</p> 
 
-	<h3>A JSON Discovery API</h3>
-	<p>
-		A simple GET/JSON-based <a href="./discovery-api/api.php">Discovery API</a> that can help when using OExchange Discovery and WebFinger together.
-	</p> 
-
-	<h3>Send-To</h3>
-	<p>
-		The <a href="./sendto">Send-To</a> service uses OExchange and WebFinger protocols together to send links around.
-	</p>
-		
-	-->
 <?
 include("../footer.php");
 ?>
