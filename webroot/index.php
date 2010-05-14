6755d6426f347445b624680870c82404a84b7017
Will Meyer <will@willmeyer.com>
2010-05-14 09:48:38 -0400

    changed dflt home page

--- webroot/index.php
+++ webroot/index.php
@@ -1,6 +1,20 @@
 <?php
-header("Location: /spec/");
+$page_title = "OExchange";
+include 'header.php';
 ?>
+<p>
+	Beautiful, informative home page coming soon.  For now, take a look at:
+	<ul>
+		<li>the <a href="/guide">Quick Start Guide</a></li> 
+		<li>some dev and test <a href="/tools">tools</a></li>
+		<li>the actual <a href="/spec">spec</a></li>
+	</ul>
+</p>
+<?php
+include 'footer.php';
+?>
+
+
 
 
 
