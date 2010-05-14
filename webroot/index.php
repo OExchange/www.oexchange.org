434b57ae4ae6698da07a55e8590905e4617c9477
Will Meyer <will@willmeyer.com>
2010-05-14 09:56:31 -0400

    changed dflt home page

--- webroot/index.php
+++ webroot/index.php
@@ -3,7 +3,10 @@ $page_title = "OExchange";
 include 'header.php';
 ?>
 <p>
-	Beautiful, informative home page coming soon.  For now, take a look at:
+	Beautiful, informative home page coming soon...
+</p>
+<p>	
+	  For now, take a look at:
 	<ul>
 		<li>the <a href="/guide">Quick Start Guide</a></li> 
 		<li>some dev and test <a href="/tools">tools</a></li>
