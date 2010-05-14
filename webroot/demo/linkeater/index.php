5289e0b0edc110758e114a5b0f28f49b3e1c58e0
Will Meyer <will@willmeyer.com>
2010-05-14 17:38:02 -0400

    cleaned up linkeater main page

--- webroot/demo/linkeater/index.php
+++ webroot/demo/linkeater/index.php
@@ -3,7 +3,7 @@
 if (isset($_GET["demo"])) $demo = $_GET["demo"];
 else $demo = false;
 
-$page_title = "LinkEater, An Example OExchange Target";
+$page_title = "LinkEater: An Example OExchange Target";
 require_once("../../header-subdemo.php");
 ?>
     <script type="text/javascript" src="/tools/badge/jquery.oexchange.js"></script>
@@ -18,25 +18,26 @@ require_once("../../header-subdemo.php");
         <div class="banner-linkeater"></div>
 	    <div class="grid_8 suffix_1 alpha">
             <p>
-        		LinkEater is a simple example of a service that accepts URLs via OExchange.  It doesn't do much with them, but hey, you get the idea.  Imagine its a social network, a news submission service, a translation tool, whatever.      
+        		LinkEater is a service that can accept URLs via OExchange.  It doesn't do much with them, but hey, its just an example.  Imagine its a social network, a news submission service, a translation tool, whatever.      
         	</p>
           
         	<h3>Sending Links to LinkEater</h3>
         	<p>
-        		Links are accepted via a standard OExchange Offer endpoint, at:
+        		It can accept links at a standard <a href="/spec/#offer">Offer</a> endpoint:
         	</p>
         	<pre>http://www.oexchange.org/demo/linkeater/offer.php</pre>	
         	<p>
         		Try <a href="offer.php?url=http://www.example.com">sending it a URL</a>.
         	</p>	
-        	<h3>Automatic Discovery</h3>
+        	<h3>Discovering LinkEater</h3>
         	<p>
-        		LinkEater is fully discoverable; any tool that understands OExchange Discovery can figure out how to send links to LinkEater automagically.
+        		Any tool that can perform <a href="/spec/#discovery">Discovery</a> can figure out how to send content to LinkEater:
         		<ul>
-        			<li>The service has a <a href="oexchange.xrd">Target XRD document</a>, which includes all of its details</li>
-        			<li>You can discover the service from just the hostname, since it also has a <a href="../../../.well-known/host-meta">/.well-known/host-meta</a> document</li>
-        			<li>You can discover the service from any page on this site, because they all have related-target page meta tags</li> 
+        			<li>There's a <a href="oexchange.xrd">Target XRD document</a>, which describes the service</li>
+        			<li>OExchange.org has a <a href="/.well-known/host-meta">/.well-known/host-meta</a>, so LinkEater is discoverable from there</li>
+        			<li>All the pages on OExchange.org also have a meta tag that identifies the service</li> 
         		</ul>
+				You can see whats discoverable from the <a href="/tools/discoveryharness/?h=www.oexchange.org&x=http%3A%2F%2Fwww.oexchange.org%2Fdemo%2Flinkeater%2Foexchange.xrd&cmd=hm">Discovery Harness</a>, too.
         	</p>
         </div>
         
