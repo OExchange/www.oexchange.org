d7998326512b13f3330d15e7a43c384ea25e65f7
foo <foo@clearspring.com>
2010-05-19 12:25:27 -0400

    Updated google buzz share link.

--- webroot/share.php
+++ webroot/share.php
@@ -1,6 +1,6 @@
         <div class="share">                        
             Share it: 
-            <a href="http://www.facebook.com/sharer.php?u=<?php echo urlencode('http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'])  ?>"><img src="/images/facebook-16px.png" border="0" alt="Share to Facebook" /></a> 
-            <a href="http://twitthis.com/twit?url=<?php echo urlencode('http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'])  ?>"><img src="/images/twitter-16px.png" border="0" alt="Share to Twitter" /></a> 
-            <a title="Post on Google Buzz" href="http://www.google.com/buzz/post" data-button-style="link"><img src="/images/google-buzz-16px.png" border="0" alt="Share to Google Buzz" /></a>
+            <a title="Share to Facebook" href="http://www.facebook.com/sharer.php?u=<?php echo urlencode('http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'])  ?>"><img src="/images/facebook-16px.png" border="0" alt="Share to Facebook" /></a> 
+            <a title="Share to Twitter" href="http://twitthis.com/twit?url=<?php echo urlencode('http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'])  ?>"><img src="/images/twitter-16px.png" border="0" alt="Share to Twitter" /></a> 
+            <a title="Post on Google Buzz" href="http://www.google.com/buzz/post?url=<?php echo urlencode('http://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'])  ?>"><img src="/images/google-buzz-16px.png" border="0" alt="Share to Google Buzz" /></a>
         </div>        
