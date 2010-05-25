30f1d65421c9f730b66900d0d24339d89de0dbbc
foo <foo@clearspring.com>
2010-05-25 11:46:19 -0400

    Added blog header include.

--- webroot/pagetop-blog.inc.php
+++ webroot/pagetop-blog.inc.php
@@ -1,16 +1,13 @@
 <?
-$stylesheets = array("/styles/960.css","/styles/site.css");
+$stylesheets = array("blog.css");
 $scriptFiles = array("http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js");
-$styleBlocks = array("body { background: #fff; padding: 0; } .container_12 { margin: 0 auto; }");
 include 'head.inc.php';
 ?>
 
 <body>
-<a name="top"></a>
 <div class="navbar">
     <div class="navbar-inner">
         <a href="/demo">Back to OExchange Demos</a>
     </div>
 </div>
-<div class="container_12">
 	
