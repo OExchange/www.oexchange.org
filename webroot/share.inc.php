d753c7c9be48b38d70cd46884bde13fd27a2e89d
mjk <matt@addthis.com>
2010-06-01 17:21:34 -0400

    Orange was preferred.

--- webroot/share.inc.php
+++ webroot/share.inc.php
@@ -8,10 +8,10 @@
     }
     </script>
     <div class="addthis_toolbox addthis_default_style">
-        <a class="addthis_button_compact"><span style="background:url(http://www.openshareicons.com/media/images/openshareicons/1/png/openshareicon-16x16.png) no-repeat left;display:block;padding-left:20px">Share</span></a>
-        <span class="addthis_separator">|</span>
         <a title="Share to Facebook" target="_blank" class="addthis_button_facebook"><img src="<?= $CFG_IMAGEBASE_URL ?>/facebook-16px.png" border="0" alt="Share on Facebook" /></a> 
         <a title="Share to Twitter" target="_blank" class="addthis_button_twitter"><img src="<?= $CFG_IMAGEBASE_URL ?>/twitter-16px.png" border="0" alt="Post to Twitter" /></a> 
         <a title="Post on Google Buzz" target="_blank" class="addthis_button_googlebuzz"><img src="<?= $CFG_IMAGEBASE_URL ?>/google-buzz-16px.png" border="0" alt="Post to Google Buzz" /></a>
+        <span class="addthis_separator">|</span>
+        <a class="addthis_button_compact">Share</a>
     </div>
 </div>        
