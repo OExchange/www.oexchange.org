09eb0cec6c304121243e478f0d86cf6c3952a268
foo <foo@clearspring.com>
2010-06-02 11:10:32 -0400

    Small tweak to Demo Wrapper

--- webroot/demo/wrappers/index.php
+++ webroot/demo/wrappers/index.php
@@ -27,15 +27,6 @@ require_once("../../pagetop-main.inc.php");
     <div class="grid_2 omega center"><a target="_blank" href="http://oexchange-buzz.appspot.com">Google Buzz<br/><img src="<?= $CFG_IMAGEBASE_URL ?>/buzz_138x104.jpg" border="0" alt="Google Buzz" class="thumb" /></a></div>
     
     <div class="clear"></div>
-    <!--
-	<ul>
-		<li><a target="_blank" href="http://oexchange-twitter.appspot.com">Twitter</a></li>
-		<li><a target="_blank" href="http://oexchange-facebook.appspot.com">Facebook</a></li>
-		<li><a target="_blank" href="http://oexchange-digg.appspot.com">Digg</a></li>
-		<li><a target="_blank" href="http://oexchange-delicious.appspot.com">Delicious</a></li>
-		<li><a target="_blank" href="http://oexchange-buzz.appspot.com">Google Buzz</a></li>
-	</ul>
-	-->
 	
 <?
 require_once("../../pagebottom.inc.php");
